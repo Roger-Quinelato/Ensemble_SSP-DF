60726f569bc340872898287e3f513b6911e49476
Roger-Quinelato <roger.quinelato@undf.edu.br>
2026-01-07 09:33:26 -0300

    docker não terminado

--- src/models/models_base.py
+++ src/models/models_base.py
@@ -5,6 +5,7 @@ from pyod.models.hbos import HBOS
 import numpy as np
 from src.utils.logger_utils import log_execution
 
+
 class BaselineModels:
     def __init__(self, X_data):
         """
@@ -13,13 +14,17 @@ class BaselineModels:
             X_data (np.ndarray ou dask.dataframe.DataFrame): Dados normalizados.
         """
         # Se for Dask DataFrame, converte para numpy para modelagem
-        if hasattr(X_data, 'compute'):
-            self.X = X_data.compute().values if hasattr(X_data, 'values') else X_data.compute()
+        if hasattr(X_data, "compute"):
+            self.X = (
+                X_data.compute().values
+                if hasattr(X_data, "values")
+                else X_data.compute()
+            )
         else:
             self.X = X_data
-        
+
     @log_execution
-    def train_iso(self, n_estimators=100, contamination='auto'):
+    def train_iso(self, n_estimators=100, contamination="auto"):
         """
         Treina o modelo Isolation Forest para detecção de anomalias.
         Args:
@@ -29,10 +34,10 @@ class BaselineModels:
             tuple: (labels, scores, modelo treinado)
         """
         model = IsolationForest(
-            n_estimators=n_estimators, 
-            contamination=contamination, 
-            n_jobs=-1, 
-            random_state=42
+            n_estimators=n_estimators,
+            contamination=contamination,
+            n_jobs=-1,
+            random_state=42,
         )
         preds = model.fit_predict(self.X)
         scores = model.score_samples(self.X)
@@ -41,7 +46,7 @@ class BaselineModels:
         return labels, scores, model
 
     @log_execution
-    def train_hbos(self, n_bins=10, contamination='auto'):
+    def train_hbos(self, n_bins=10, contamination="auto"):
         """
         Treina o modelo Histogram-Based Outlier Score (HBOS) para detecção de anomalias.
         Args:
@@ -50,15 +55,11 @@ class BaselineModels:
         Returns:
             tuple: (labels, scores, modelo treinado)
         """
-            results = {}
-            for n_bins in [10, 20, 30]:
-                model = HBOS(n_bins=n_bins, contamination=contamination)
-                model.fit(self.X)
-                scores = model.decision_scores_
-                labels = model.labels_
-                results[n_bins] = {
-                    'labels': labels,
-                    'scores': scores,
-                    'model': model
-                }
-            return results
+        results = {}
+        for n_bins in [10, 20, 30]:
+            model = HBOS(n_bins=n_bins, contamination=contamination)
+            model.fit(self.X)
+            scores = model.decision_scores_
+            labels = model.labels_
+            results[n_bins] = {"labels": labels, "scores": scores, "model": model}
+        return results
